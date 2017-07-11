1eb61134f4d5cd5dbf6d190d8c54891ecf231354
Andy <abmoore92@gmail.com>
2017-07-11 16:52:32 +0100

    append data now creates a new file with headers if there is no existing file new column in output data "last_updated" now has datetime formatted last update time. may be different to retrieve time if propertypartner does not update every 5 minutes

--- pulldata.py
+++ pulldata.py
@@ -1,22 +1,35 @@
 import pandas as pd
 import time
 import datetime
+import os
 
 def write_today(df):
     date = time.strftime("%Y-%m-%d")
     df.to_csv('Properties_'+date+'.csv')
 
-def append_today(df):
-    df.to_csv('Properties_All.csv', mode='a', header=False)
+#append dataframe to file or make new file with header if no file exists
+def append_now(df,filename):
+    if os.path.exists(filename):
+        df.to_csv(filename, mode='a', encoding='utf-8', header=False)
+    else:
+        df.to_csv(filename, mode='a', encoding='utf-8', header=True)
 
 def read_datasource_time(source_string):
     return datetime.datetime.strptime(source_string[21:-15],'%b %d %Y %H:%M:%S')
 
 
 url = 'https://docs.google.com/spreadsheets/d/19h2GmLN-2CLgk79gVxcazxtKqS6rwW36YA-qvuzEpG4/export?format=xlsx'
-df = pd.read_excel(url, header=1).rename(columns={'Unnamed: 1':'code'})
+df = pd.read_excel(url, header=1).rename(columns={'Unnamed: 1':'code','Unnamed: 12':'URL'})
+
 source = pd.read_excel(url).columns[0]
 df['source'] = source
-df['time'] = datetime.datetime.now()
-#write_today(df)
-append_today(df)
+
+#datetime column of when the data was most recently updated
+df['last_updated'] = read_datasource_time(source)
+
+#datetime column of the time when the script is run
+df['retrieve_time'] = datetime.datetime.now()
+
+#write this entry
+filename = 'Properties_All.csv'
+append_now(df,filename)
